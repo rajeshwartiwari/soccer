b90af2bdf19a6e8b7649969b63a7e7a5d492a335
Rajeshwar Tiwari <rajeshwar.tiwari@tcs.com>
2020-07-03 15:30:06 +0100

    first comment

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -17,7 +17,7 @@ constructor(props){
 }
 
 componentDidMount(){
-  const url= 'http://localhost:4001/players';
+  const url= 'http://backend-service:4001/players';
   axios.get(url)
   .then((Response) => {
     this.setState({
